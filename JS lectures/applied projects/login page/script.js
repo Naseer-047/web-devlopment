8583f042805767061a5ef73825c317f5e8066471
Naseer-047 <n763122562@gmail.com>
2025-09-10 16:23:55 +0530

    ------

--- JS lectures/applied projects/login page/script.js
+++ JS lectures/applied projects/login page/script.js
@@ -7,12 +7,18 @@
           document.getElementById('msg')
         msg.innerText=' ✅Login success full';
           msg.style.color='green'
-            // document.getElementById('box').style.display = 'none';
+            document.getElementById('box').style.display = 'none';
+            document.getElementById('cb').style.display = 'block';
+            document.getElementById('use').innerText=userName;
+            document.getElementById('pass').innerText=password;
          } else {
            
             document.getElementById('msg')
         msg.innerText='❌ Wrong Username or Password';
           msg.style.color='red'
+
+        
+         
          }
          
         }
